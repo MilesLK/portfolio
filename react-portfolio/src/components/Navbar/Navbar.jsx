d98142ff4e8f1861e12256f57f8269d6390e92b6
MilesLK <bricefrancietta@hotmail.com>
2024-06-14 16:01:10 +0200

    comiit 2714

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react';
+import styles from "./Navbar.module.css";
+import {getImageUrl} from "../../utils";
 
-const Navbar = () => {
-  return <div>Navbar</div>
-}
+export const Navbar = () => {
+  return (
+  <nav className={styles.navbar}>
+    <a className={styles.title} href="/">Portfolio</a>
+    <div className={styles.menu}>
+      <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt='menu-button'/>
+      <ul className={styles.menuItems}>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#about">Experience</a>
+        </li>
+        <li>
+          <a href="#about">Projects</a>
+        </li>
+        <li>
+          <a href="#about">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  );
+};
