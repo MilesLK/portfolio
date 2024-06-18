ccaeb0700a8c28a6e13605c5a10c337d53db9b8f
MilesLK <bricefrancietta@hotmail.com>
2024-06-18 18:28:12 +0200

    commit 4641

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar';
+import { Hero } from './components/Navbar/Hero/Hero';
 
 function App() {
   return  ( <div className={styles.App}>
     <Navbar />
-  </div> 
+    <Hero />
+  </div>
   );
 }
 
---
+++ react-portfolio/src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { getImageUrl } from "../../../utils";
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+    return  <section className={styles.container}>
+        <div className={styles.content}> 
+        <h1 className={styles.title}> Hi, Je suis Brice</h1>
+        <p className={styles.description}> Je suis un développeur fullstack avec 1 an d'experience j'utilise react, utilise la barre de navigation si tu veux en savoir plus à mon sujet </p>
+        <a className={styles.contactBtn} href="mailto:bricefrancietta@hotmail.com"> Me contacter </a>        
+        </div>
+        <img className={styles.heroImg} src={getImageUrl('hero/heroImage.png')} alt="Ma photo" />
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+    </section>;
+}
